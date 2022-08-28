fbd2a47db8d86cb105a689b10dc5073b4245cdc9
rid50 <rdavidenko@gmail.com>
2022-08-28 21:23:07 +0300

    first commit

---
+++ GetStringAsync/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text.RegularExpressions;
+class Program
+{
+    static async Task Main(string[] args) {
+        await GetString();
+    }
+
+    static readonly HttpClient _httpClient = new HttpClient();
+
+    private static async Task<int> GetString()
+    {
+        //private static readonly HttpClient _httpClient2 = new HttpClient();
+        // Capture the task handle here so we can await the background task later.
+        var str =  _httpClient.GetStringAsync("https://dotnetfoundation.org");
+
+        // The await operator suspends OnSeeTheDotNetsButtonClick(), returning control to its caller.
+        // This is what allows the app to be responsive and not block the UI thread.
+        //var str2 = await str;
+        var str2 = str.Result;
+
+        int count = Regex.Matches(str2, @"\.NET").Count;
+
+        Console.WriteLine($"Number of .NETs on dotnetfoundation.org: {count}");
+            
+        return 0;
+    }
+}
---
+++ Linq/Tutorialspoint/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] words = new [] { "hell", "wonderful", "LINQ", "beautiful", "world" };
+
+        //Get only short words
+        var shortWords = from word in words where word.Length <= 5 select word;
+
+        //Print each word out
+        foreach (var word in shortWords)
+        {
+            Console.WriteLine(word);
+        }
+
+        // Specify the data source.
+        int[] scores = { 97, 92, 81, 60 };
+
+        // Define the query expression.
+        IEnumerable<int> scoreQuery = from score in scores where score > 80 select score;
+
+        // Execute the query.
+
+        foreach (int i in scoreQuery)
+        {
+            Console.Write(i + " ");
+        }
+
+        //Console.ReadLine();
+    }
+}
